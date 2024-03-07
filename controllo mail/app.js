2da80242b2a5ff3ccff251516641c14807aba9f5
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-07 15:52:57 +0100

    ADD: struttura js delle mail

--- controllo mail/app.js
+++ controllo mail/app.js
@@ -1,10 +1,25 @@
 // chiedere l'inserimento della mail dell'utente
     // inserire pulsante per trasmettere i dati e confrontarli con l'array
-    // prendere i dati inseriti da un input di tipo "mail"
+    const verifyElementDOM = document.getElementById('verify');
 
-// controllare se la mail è tra quele per l'accesso
+    // prendere i dati inseriti da un input di tipo "mail"
+    const userMailElementDOM = document.getElementById('mail');
+
+// al click del pulsante controllare se la mail è tra quele per l'accesso
+
+    // esplicitare il click e la sua rispettiva azione 
+    verifyElementDOM.addEventListener('click', function(){
+        console.log(userMailElementDOM.value);
+    })
+
     // esplicitare un array con delle mail valide
+    const validMails = ['donato@gmail.com', 'samuel@gmail.com', 'gianluca@gmail.com'];
+
     // confrontare la mail ricevuta con la lista delle mail valide
+    for (let i = 0, i < validMails.length, i++) {
+        const mails = validMails[i];
+        console.log(mails);
+    }
 
 // restituire messaggio positivo o negativo
     // se la mail corrisponde
