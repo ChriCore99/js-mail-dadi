7437af098ed2a673ebbf3186f8c93cb018fedad0
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-07 15:29:35 +0100

    ADD: strutture base esercizio mail

---
+++ controllo mail/app.js
@@ -0,0 +1,13 @@
+// chiedere l'inserimento della mail dell'utente
+    // inserire pulsante per trasmettere i dati e confrontarli con l'array
+    // prendere i dati inseriti da un input di tipo "mail"
+
+// controllare se la mail è tra quele per l'accesso
+    // esplicitare un array con delle mail valide
+    // confrontare la mail ricevuta con la lista delle mail valide
+
+// restituire messaggio positivo o negativo
+    // se la mail corrisponde
+        // restituire messaggio di approvazione
+    // altimenti
+        // mandere messaggio di accesso negato
