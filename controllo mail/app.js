5d565da4d52586cd7129be9497d3496ef6bd055e
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-07 17:14:52 +0100

    ADD: fine codice e fix bag

--- controllo mail/app.js
+++ controllo mail/app.js
@@ -1,28 +1,38 @@
 // chiedere l'inserimento della mail dell'utente
-    // inserire pulsante per trasmettere i dati e confrontarli con l'array
-    const verifyElementDOM = document.getElementById('verify');
+// inserire pulsante per trasmettere i dati e confrontarli con l'array
+const verifyElementDOM = document.getElementById("verify");
 
-    // prendere i dati inseriti da un input di tipo "mail"
-    const userMailElementDOM = document.getElementById('mail');
+// prendere i dati inseriti da un input di tipo "mail"
+const userMailElementDOM = document.getElementById("mail");
+
+// esplicitare un array con delle mail valide
+const validMails = [
+  "donato@gmail.com",
+  "samuel@gmail.com",
+  "gianluca@gmail.com",
+];
+let verifiedValidMails = false;
 
 // al click del pulsante controllare se la mail è tra quele per l'accesso
 
-    // esplicitare il click e la sua rispettiva azione 
-    verifyElementDOM.addEventListener('click', function(){
-        console.log(userMailElementDOM.value);
-    })
+// esplicitare il click e la sua rispettiva azione
+verifyElementDOM.addEventListener("click", function () {
+  const userMail = userMailElementDOM.value;
 
-    // esplicitare un array con delle mail valide
-    const validMails = ['donato@gmail.com', 'samuel@gmail.com', 'gianluca@gmail.com'];
-
-    // confrontare la mail ricevuta con la lista delle mail valide
-    for (let i = 0, i < validMails.length, i++) {
-        const mails = validMails[i];
-        console.log(mails);
+  // confrontare la mail ricevuta con la lista delle mail valide
+  for (let i = 0; i < validMails.length; i++) {
+    // restituire messaggio positivo o negativo
+    if (userMail === validMails[i]) {
+      verifiedValidMails = true;
     }
+  }
 
-// restituire messaggio positivo o negativo
+  if (verifiedValidMails === true) {
     // se la mail corrisponde
-        // restituire messaggio di approvazione
+    // restituire messaggio di approvazione
+    console.log("E-Mail accettata");
+  } else {
     // altimenti
-        // mandere messaggio di accesso negato
+    console.log("E-Mail non valida. Riprova"); // mandere messaggio di accesso negato
+  }
+});
